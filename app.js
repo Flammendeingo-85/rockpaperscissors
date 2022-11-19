a4ff5abcc6962f0047b05e747e7bdde2c44fa627
Flammendeingo-85 <Ravi.Banwari1108@gmail.com>
2022-11-20 00:59:43 +0100

    added some .js-code

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const ComputerChoice = document.getElementById('computer-choice')
-const UserChoice = document.getElementById('user-choice')
-const ResultDisplay = document.getElementById('result')
+const ComputerChoiceDisplay = document.getElementById("computer-choice");
+const UserChoiceDisplay = document.getElementById("user-choice");
+const ResultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+let computerChoice;
+
+possibleChoices.forEach((possibleChoices) =>
+  possibleChoices.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    UserChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+  })
+);
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3); //or you can use possibleChoices.length
+
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "scissors";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "paper";
+  }
+  ComputerChoiceDisplay.innerHTML = computerChoice;
+}
